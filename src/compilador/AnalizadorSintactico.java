f6830977c24c0ba61d364f10858478ba0e936037
Alan Jimenez <aljiar23@gmail.com>
2018-10-21 15:05:27 -0400

    Base del sintactico

--- src/compilador/AnalizadorSintactico.java
+++ src/compilador/AnalizadorSintactico.java
@@ -1,18 +1,55 @@
 package compilador;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class AnalizadorSintactico {
-	
+	private ArrayList<Rule> rules = new ArrayList<Rule>();
 	public AnalizadorSintactico()
 	{
 		
+		rules.add(new Rule("M","(M)"));
+		rules.add(new Rule("M","M^N"));
+		rules.add(new Rule("M","M*N"));
+		rules.add(new Rule("M","M/N"));
+		rules.add(new Rule("M","E+M"));
+		rules.add(new Rule("M","E-M"));
+		rules.add(new Rule("E","M"));
+		rules.add(new Rule("M","N"));
+		
 	}
-	public boolean validateRule(String rule, ArrayList<String> text)
+	public boolean validateRule(String toAnalize)
 	{
+		String aux="";
 		boolean res= false;
+		for(int index=0; index<toAnalize.length();index++)
+		{
+			aux=aux+toAnalize.charAt(index);
+			for(int r_index=0;r_index<rules.size();r_index++){
+				System.out.println(aux);
+				if(aux.equals(rules.get(r_index).getValue()))
+				{
+					res=true;
+					}
+				else {
+					
+					res=false;
+				}
+			}
+		}
 		
 		return res;
+	}
+	public String changeString(String toAnalize)
+	{
+		String res="";
+		for(int index=0;index<rules.size();index++)
+		{
+			//for(int ind_r=) {
+			//if(toAnalize.charAt(index).equals())
+			//}
+		}
+		return res;
+		
 	}
 
 }
--- src/compilador/MainCompilador.java
+++ src/compilador/MainCompilador.java
@@ -12,11 +12,12 @@ public class MainCompilador {
 		test();
 	}
 	public static void test() throws FileNotFoundException, IOException {
+		AnalizadorSintactico sintactico= new AnalizadorSintactico(); 
 		LexicoCompilador tknCS;
 		tknCS = new LexicoCompilador();
 		ArrayList<String> cadena = new ArrayList<String>();
 		String txt;
-		ArrayList<String> changed = new ArrayList<String>(); 
+		String toSintactico="";
 		txt = readfile("example_code.txt");
 		StringTokenizer tkn = new StringTokenizer(txt);
 		while (tkn.hasMoreTokens()) {
@@ -27,41 +28,34 @@ public class MainCompilador {
 		while (j < cadena.size()) {
 			if (tknCS.isKeyword(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es palabra reservada");
-				changed.add(cadena.get(j));
 			} else if (tknCS.isSystemKeyWord(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Pal.Res. Libreria System");
-				changed.add(cadena.get(j));
 			} else if (tknCS.isComparador(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Comparador");
-				changed.add(cadena.get(j));
 			} else if (tknCS.isDelimitador(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Delimitador");
-				changed.add(cadena.get(j));
-			}else if (tknCS.isSuma(cadena.get(j))) {
-				System.out.println(cadena.get(j) + " Es suma");
-				changed.add(cadena.get(j)); 
+				toSintactico=toSintactico+cadena.get(j);
 			} else if (tknCS.isCaracterEspecial(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Caracter Especial");
-				changed.add(cadena.get(j));
 			} else if (tknCS.isComentario(cadena.get(j))) {
 				System.out.println("");
 			} else if (tknCS.isCadena(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Cadena");
-				changed.add(cadena.get(j));
 			} else if (tknCS.isIdentifier(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Identificador");
-				changed.add(cadena.get(j));
 			} else if (tknCS.isOperador(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Operador");
-				changed.add(cadena.get(j));
+				toSintactico=toSintactico+cadena.get(j);
 			} else if (tknCS.isEntero(cadena.get(j))) {
 				System.out.println(cadena.get(j) + " Es Entero");
-				changed.add("N");
+				toSintactico=toSintactico+"N";
 			} else {
 				System.out.println(cadena.get(j) + " Definicion invalida");
 			}
 			j = j + 1;
 		}
+		boolean res=sintactico.validateRule(toSintactico);
+		System.out.println(toSintactico+" Valido: "+res);
 	}
 	public static String readfile(String path) throws FileNotFoundException,IOException {
 		
